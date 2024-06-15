50edd3e6bf9fcce4a5186edec1487f005569a946
chinpeerapat <88846925+Chinpeerapat@users.noreply.github.com>
2024-06-15 17:13:00 +0700

    Update theme.ts

--- src/documents/theme.ts
+++ src/documents/theme.ts
@@ -16,7 +16,7 @@ export function createTheme(accentColor = "#2B5DD6", baseFontSize = 10) {
     //      0  1  2  3   4   5   6   7   8   9  10  11  12
     space: [2, 4, 6, 8, 10, 12, 14, 16, 20, 22, 24, 28, 32],
 
-    lineHeight: 1.4,
+    lineHeight: 1.15,
 
     fontSize: [
       clampedBaseFontSize,
